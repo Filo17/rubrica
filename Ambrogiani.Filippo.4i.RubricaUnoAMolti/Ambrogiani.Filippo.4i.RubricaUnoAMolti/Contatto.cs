5a4f1ae1afb5336506cf2a8de6e3c9d7e2f0a17f
Filippo Ambrogiani <81953792+Filo17@users.noreply.github.com>
2024-02-07 10:52:19 +0100

    Add files via upload

---
+++ Ambrogiani.Filippo.4i.RubricaUnoAMolti/Ambrogiani.Filippo.4i.RubricaUnoAMolti/Contatto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambrogiani.Filippo._4i.Rubrica
+{
+    public enum TipoContatto { nessuno, Email, Telefono, Web, Instagram, Facebook, Cellulare}
+    public class Contatto
+    {
+        public int idPersona { get; set; }
+        public TipoContatto Tipo { get; set; }
+        public string Valore { get; set; }
+
+
+        public Contatto()
+        {
+            Tipo = TipoContatto.nessuno;
+        }
+
+        public Contatto(string riga)
+        {
+            string[] Campi = riga.Split(";"); //divido una riga csv
+
+            if (Campi.Count() != 3)
+                throw new Exception("Le righe del file Contatti.csv devono essere di 3 colonne");
+
+            int id = 0;
+            int.TryParse(Campi[0], out id);
+            idPersona = id;
+
+            TipoContatto c;
+            Enum.TryParse(Campi[1], out c);
+            this.Tipo = c;
+
+            this.Valore = Campi[2];
+
+        }
+    }
+}
